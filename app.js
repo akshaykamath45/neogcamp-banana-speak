334a1e315234c68e2a6aa70c6d798b14204ff2d5
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2022-11-16 02:12:19 +0530

    CSS part left

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
-
+//var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL="https://api.funtranslations.com/translate/minion.json"
 // outputDiv.innerText="Akshay Kamath "
 
+function getTranslationURL(input){
+    return serverURL+"?"+"text="+input
+}
+
+function errorHandler(error){
+    console.log("Error Occured",error)
+}
 function clickHandler(){
-    outputDiv.innerText="sadddvsscd "+txtInput.value;
+
+    var inputText=txtInput.value;//taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json=> {
+        var translatedText=json.contents.translated;
+        outputDiv.innerText=translatedText;//output
+    })
+    .catch(errorHandler)
 };
 btnTranslate.addEventListener("click",clickHandler)
 
